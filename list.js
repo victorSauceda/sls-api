94e994df8348b6c83c81157a27d54aa28d5ffd9b
Victor <victorasauceda@gmail.com>
2020-01-22 10:58:45 -0800

    adding our severless API

---
+++ list.js
@@ -0,0 +1,18 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response=lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeVaulues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    return success(result.Items);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
